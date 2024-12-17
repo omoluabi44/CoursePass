0d84435b9fbe67c664e75d76c2cba3659cc2aab6
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-12-17 23:58:05 +0100

    implement the card for courses listing

--- coursepass/app/dashboard/page.tsx
+++ coursepass/app/dashboard/page.tsx
@@ -1,31 +1,12 @@
 import React from 'react';
-import styles from './Dashboard.module.css'; // Import the styles
+import { Dropdown } from './Dropdown/Dropdown'
+import { Courses } from './Courses/Courses';
 
 const Dashboard = () => {
   return (
     <>
-    <div className={styles.container}>
-      <div className={styles.dropDownItems}>
-        <select className={styles.Items}> 
-        <option value="">-- Select Department --</option>
-            <option value="Computer Science">Computer Science</option>
-            <option value="Mechanical Engineering">Mechanical Engineering</option>
-            <option value="Business Administration">Business Administration</option>
-        </select>
-        <select className={styles.Items}> 
-        <option value="">-- Select Department --</option>
-            <option value="Computer Science">Computer Science</option>
-            <option value="Mechanical Engineering">Mechanical Engineering</option>
-            <option value="Business Administration">Business Administration</option>
-        </select>
-         <select className={styles.Items}> 
-        <option value="">-- Select Department --</option>
-            <option value="Computer Science">Computer Science</option>
-            <option value="Mechanical Engineering">Mechanical Engineering</option>
-            <option value="Business Administration">Business Administration</option>
-        </select>
-      </div>
-    </div>
+    <Dropdown/>
+    <Courses/>
     </>
   );
 };
