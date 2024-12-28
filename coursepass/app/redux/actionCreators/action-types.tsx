fe175e9576901b0a47f0529c3e81f5c1f74d8f45
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-12-28 21:11:16 +0100

    restructure my file to frontend and backend

--- coursepass/app/redux/actionCreators/action-types.tsx
+++
@@ -1,4 +0,0 @@
-export const ActionTypes ={
-    SET_COURSES: 'SET_COURSES',
-    SELECTED_COURSE: 'SELECTED_COURSE',
-}
--- coursepass/app/redux/reducer/index.jsx
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux';
-import {courseReducers} from '../reducer/courseReducers';
-
-export const rootReducer = combineReducers({
-    allCourses: courseReducers,
-})
