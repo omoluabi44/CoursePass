0abbb146aa9ca3a86bc0f4b1ac62aa8f1f798e5e
omoluabi44 <emmanuelogunleye441999@gmail.com>
2025-01-08 21:05:43 +0100

    change the sidebar and also make it collapseable

--- Frontend/src/component/Sidebar/Sidebar.jsx
+++ Frontend/src/component/Sidebar/Sidebar.jsx
@@ -1,55 +1,55 @@
 import React, { useState } from 'react';
-import s from "./Sidebar.module.css"; // Import your global CSS file
+import s from "./Sidebar.module.css"; 
+import {Link} from "react-router-dom";
 
 
 
 const Sidebar = () => {
-  const [activeDropdown, setActiveDropdown] = useState(null);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-  const handleDropdownToggle = (dropdown) => {
-    setActiveDropdown(activeDropdown === dropdown ? null : dropdown);
-  };
+
+
+  const handleToggleSidebar= () =>{
+    setIsCollapsed(!isCollapsed)
+
+  }
+
 
   return (
-    <aside id={s.sidebar}>
-      <ul>
-        <li className={s.active}>
-          <a href="/dashboard">
-            <svg className={s.svg} 
-            xmlns="http://www.w3.org/2000/svg" 
-            height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
-            <path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
-            </svg>
+    <aside id={s.sidebar} className={isCollapsed ? s.collapsed : null}>
+     <div className={s.logo} >
+      <span>CoursePass</span>
+      <svg
+      onClick={handleToggleSidebar}
+       xmlns="http://www.w3.org/2000/svg" 
+       height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z"/></svg>
+     </div>
+     <ul className={s.sidebar_menu}>
+      <li >
+     
+      <Link to="/dashboard">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M520-600v-240h320v240H520ZM120-440v-400h320v400H120Zm400 320v-400h320v400H520Zm-400 0v-240h320v240H120Zm80-400h160v-240H200v240Zm400 320h160v-240H600v240Zm0-480h160v-80H600v80ZM200-200h160v-80H200v80Zm160-320Zm240-160Zm0 240ZM360-280Z"/></svg>
             <span>Dashboard</span>
-          </a>
+      </Link> 
+      
         </li>
-        
-        <li>
-          <a href="Assignment">
-            <svg className={s.svg}  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
-              <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h168q13-36 43.5-58t68.5-22q38 0 68.5 22t43.5 58h168q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm80-80h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Zm200-190q13 0 21.5-8.5T510-820q0-13-8.5-21.5T480-850q-13 0-21.5 8.5T450-820q0 13 8.5 21.5T480-790ZM200-200v-560 560Z"/>
-            </svg>
-            <span>Assignment</span>
-          </a>
+      <li>
+        <Link to="/past-questions">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M560-360q17 0 29.5-12.5T602-402q0-17-12.5-29.5T560-444q-17 0-29.5 12.5T518-402q0 17 12.5 29.5T560-360Zm-30-128h60q0-29 6-42.5t28-35.5q30-30 40-48.5t10-43.5q0-45-31.5-73.5T560-760q-41 0-71.5 23T446-676l54 22q9-25 24.5-37.5T560-704q24 0 39 13.5t15 36.5q0 14-8 26.5T578-596q-33 29-40.5 45.5T530-488ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z"/></svg>
+          <span>Pasts Questions</span>
+
+        </Link></li>
+      <li>
+        <Link to="/chat">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/></svg>
+            <span>
+            Chat with Ai
+            </span>
+        </Link>
+       
         </li>
-        <li>
-          <a href="/courseSelection">
-            <svg className={s.svg}  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
-              <path d="M560-360q17 0 29.5-12.5T602-402q0-17-12.5-29.5T560-444q-17 0-29.5 12.5T518-402q0 17 12.5 29.5T560-360Zm-30-128h60q0-29 6-42.5t28-35.5q30-30 40-48.5t10-43.5q0-45-31.5-73.5T560-760q-41 0-71.5 23T446-676l54 22q9-25 24.5-37.5T560-704q24 0 39 13.5t15 36.5q0 14-8 26.5T578-596q-33 29-40.5 45.5T530-488ZM320-240q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z"/>
-            </svg>
-            <span>Past Questions</span>
-          </a>
-        </li>
-        
-        <li>
-          <a href="Chat with AI">
-            <svg className={s.svg} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368">
-              <path d="M240-400h320v-80H240v80Zm0-120h480v-80H240v80Zm0-120h480v-80H240v80ZM80-80v-720q0-33 23.5-56.5T160-880h640q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H240L80-80Zm126-240h594v-480H160v525l46-45Zm-46 0v-480 480Z"/>
-            </svg>
-            <span>Chat with AI</span>
-          </a>
-        </li>
-      </ul>
+     </ul>
+    
     </aside>
   );
 };
